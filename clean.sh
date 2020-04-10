9dc5d4c40f6e27b3df08d3d9658e989230c131bd
cezarmathe <me@cezarmathe.com>
2020-04-10 12:18:43 +0300

    Fix aliases

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ DIRS=("${HOME}" "${HOME}/.config")
 
 for dir in ${DIRS[*]}; do
     printf "%s\n" "Cleaning ${dir}"
-    for symlink in $(find -P ${dir} -maxdepth 1 -xtype l); do
+    for symlink in $(find -P ${dir} -maxdepth 1 -type l); do
         printf "%s\n" "Removing ${symlink}"
         rm "${symlink}"
     done
