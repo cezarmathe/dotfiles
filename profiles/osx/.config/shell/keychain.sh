7590b79f3abc240f719935c88bfe2c440ef852c7
cezarmathe <me@cezarmathe.com>
2020-04-11 20:07:24 +0300

    Change osx keychain

--- profiles/osx/.config/shell/keychain.sh
+++ profiles/osx/.config/shell/keychain.sh
@@ -1,4 +1,3 @@
-eval $(keychain --agents ssh,gpg -q --eval id_rsa ~/.ssh/id_rsa)
-eval $(keychain --agents ssh,gpg -q --eval github ~/.ssh/github)
+eval $(keychain --agents ssh,gpg -q --eval id_cezarmathembp ~/.ssh/id_cezarmathembp)
 
 eval $(keychain --agents ssh,gpg -q --eval F7A4665006757D40230D107A5C6E240A90B96553)
